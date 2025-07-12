4b73ac12655dfb3dc4c26582ccee9ab348d8d019
mindovermattr <dduim@bk.ru>
2025-07-12 22:10:00 +0300

    move husky to root

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,6 +1,6 @@
 import { Outlet } from "react-router";
 
-export const App = () => {
+export const app = () => {
   return (
     <div className="flex min-h-screen flex-col">
       <Outlet />
--- src/App.tsx
+++
@@ -1,9 +0,0 @@
-import { Outlet } from "react-router";
-
-export const App = () => {
-  return (
-    <div className="flex min-h-screen flex-col">
-      <Outlet />
-    </div>
-  );
-};
