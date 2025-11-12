bc9f2ab55c15fb7c28b759120f70f7e901362c7b
mindovermattr <dduim@bk.ru>
2025-11-12 08:28:30 +0300

    improve readme files

--- client/src/pages/analytic/analytic.tsx
+++ client/src/pages/analytic/analytic.tsx
@@ -1,5 +1,17 @@
+import { SidebarTrigger } from "@/components/ui/sidebar";
+import { Typography } from "@/components/ui/typography";
+
 const Analytic = () => {
-  return <div>Analytic</div>;
+  return (
+    <div>
+      <header className="flex items-center gap-2">
+        <SidebarTrigger />
+        <Typography tag="h1" variant="title">
+          Аналитика
+        </Typography>
+      </header>
+    </div>
+  );
 };
 
 export { Analytic };
